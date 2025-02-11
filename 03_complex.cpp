0e0c92a62eb6188fddb6535e3113fb33ffa43708
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2025-02-11 04:10:21 +0000

    extra fun

--- 03_complex.cpp
+++ 03_complex.cpp
@@ -1,3 +1,4 @@
+//Adityabhise setc13
 #include<iostream>
 using namespace std;
 
@@ -24,12 +25,32 @@ class complex{;
 
     }
 
+    //subtraction
+    void subcmplx(complex c1, complex c2){
+        real = c1.real - c2.real;
+        imag = c1.imag - c2.imag;
+        cout<<real<<" - "<<"i"<<imag<<endl;
+    }
 
+    //multiplication
+    void mulcmplx(complex c1, complex c2){
+        real = c1.real * c2.real - c1.imag * c2.imag;
+        imag = c1.real * c2.imag + c1.imag * c2.real;
+        cout<<real<<" + "<<"i"<<imag<<endl;
+    }
+
+    //division
+    void divcmplx(complex c1, complex c2){
+        int denom = c2.real * c2.real + c2.imag * c2.imag;
+        real = (c1.real * c2.real + c1.imag * c2.imag) / denom;
+        imag = (c1.imag * c2.real - c1.real * c2.imag) / denom;
+        cout<<real<<" + "<<"i"<<imag<<endl;
+    }
 
 };
 
 int main(){
-    complex c1,c2,c3;
+    complex c1,c2,c3,c4,c5,c6;
     int a,b,c,d;
     cout << "Enter the real and imaginary part of the first complex number: ";
     cin >> a >> b;
@@ -41,11 +62,23 @@ int main(){
     c1.setcmplx(a,b);
     c2.setcmplx(c,d);
     c3.addcmplx(c1,c2);
+    c4.subcmplx(c1,c2);
+    c5.mulcmplx(c1,c2);
+    c6.divcmplx(c1,c2);
+
     cout<<"c1 = ";
     c1.getcmplx();
     cout<<"c2 = ";
     c2.getcmplx();
     cout<<"c3 = ";
     c3.getcmplx();
+    cout<<"c4 = ";
+    c4.getcmplx();
+    cout<<"c5 = ";
+    c5.getcmplx();
+    cout<<"c6 = ";
+    c6.getcmplx();
+    
+    
     return 0;
 }
